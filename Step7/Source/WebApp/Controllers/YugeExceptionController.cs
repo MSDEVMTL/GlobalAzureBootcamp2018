0e1a697ae4843448642f5426f8eb7bcfa9711641
Mathieu Richard <mathieu.richard5@gmail.com>
2018-04-14 15:48:12 -0400

    enabling of Yuge exception

---
+++ Step7/Source/WebApp/Controllers/YugeExceptionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class YugeExceptionController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(YugeModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
